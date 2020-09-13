69e7562156e406cc3deb612592c1deb72d0c7a9b
Christina Howard <clhoward33@icloud.com>
2020-09-13 21:09:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc
+end
 
+def sort_array_desc
+end
+
+def sort_array_chair_count
+end
+
+def swap_elements
+end
+
+def reverse_array
+end
+
+def kesha_maker
+end
+
+def find_a 
+end
+
+def sum_array
+end
+
+def add_s
+end
