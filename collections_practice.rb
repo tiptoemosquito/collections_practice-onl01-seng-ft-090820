af2367988ea7a6efd2c8477c97dd2e53187d052b
Christina Howard <clhoward33@icloud.com>
2020-09-13 21:40:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,17 +8,20 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-
+  array.sort do |left, right|
+    left.length <=> right.length
 end
 
 def swap_elements(array)
+  array [1], array [2] = array [2], array [1]
 end
 
 def reverse_array(array)
-  array.reverse 
+  array.reverse
 end
 
 def kesha_maker(array)
+
 end
 
 def find_a(array)
